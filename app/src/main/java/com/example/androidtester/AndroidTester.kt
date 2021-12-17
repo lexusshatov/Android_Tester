0bdaf0b10e798fd2f576771ef84ba1f912a18665
Oleh Shatov <oleg.shatov.natife@gmail.com>
2021-12-17 16:51:00 +0200

    settings for hilt, manifest

---
+++ app/src/main/java/com/example/androidtester/AndroidTester.kt
@@ -0,0 +1,7 @@
+package com.example.androidtester
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AndroidTester : Application()
--- app/src/main/java/com/example/androidtester/MainActivity.kt
+++ app/src/main/java/com/example/androidtester/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.androidtester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
